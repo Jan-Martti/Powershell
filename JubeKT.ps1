29d7e8970d1ae26bd02aa97b818254bdea2fda3e
Jan-Martti <115462243+Jan-Martti@users.noreply.github.com>
2023-01-19 21:17:46 +0200

    Add files via upload

---
+++ JubeKT.ps1
@@ -0,0 +1,36 @@
+﻿#Jan-Martti Olop
+#KT 04
+#01.19.2023                                                          
+#-----------------------------------------------------------# 
+# Skript tahab teada kasutajalt faili nime                  #
+# Siis skript võtab xml faili sisu                          #
+# Skript iga reaga võtab nime muutujasse                    #
+# Paska kasutatakse kaustade ja failide loomiseks           #
+#-----------------------------------------------------------#
+
+$Skriptitee = $MyInvocation.MyCommand.Path
+$dir = Split-Path $Skriptitee
+
+$sisestus = Read-Host "Mis on fail mida tahate avada? (*.xml)"
+
+
+if ($sisestus -notmatch "$_.xml")
+{
+    DO
+    {
+        Write-Host "Error: Valen fail nuub!!!!!!"
+        $sisestus = Read-Host "Palun sisestage faili nimi uuesit see eelmine oli vale!!!!!!!"
+    }
+   Until ($sisestus -match "$_.xml")
+}
+
+$xml = [xml](Get-Content $dir\$sisestus)
+$rows = $xml.customers.customer
+
+foreach ($row in $rows)
+{
+    $pask = $row.full_name
+    New-Item $dir\customers\$pask -ItemType Directory
+    new-item $dir\customers\$pask\$pask.txt
+    $row >> $dir\customers\$pask\$pask.txt
+}
---
+++ yl6.ps1
@@ -0,0 +1,27 @@
+﻿$nimi = "Jan-Martti Olop"
+$email = "jannu.olop@gmail.com"
+$kp = get-date -Format "dd.MM.yy"
+
+
+$nimi
+$email
+$kp
+
+
+
+$skriptiAsukoht = $MyInvocation.MyCommand.Path
+$dir = Split-Path $skriptiAsukoht
+$emailid = Get-Content -path $dir\emailid.txt
+
+
+$kasutajad = $emailid.split(",")
+$kasutajad+= $email
+"Esimene kasutaja on "+$kasutajad[0]
+"Viimane kasutaja on "+$kasutajad[-1]
+"Kasutajad kokku on "+$kasutajad.Length
+
+
+
+
+
+
